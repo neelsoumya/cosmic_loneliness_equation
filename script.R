46ae98c703b0ac0ef72a737e8c173d126fba5674
soumyabanerjee <soumyabanerjee@soumyabanerjees-MacBook-Pro.local>
2022-09-11 14:39:02 +0100

    ed

--- script.R
+++ script.R
@@ -5,6 +5,12 @@
 # advanced radio transmitting civilization
 ##################################################
 
+###########################
+# load libraries
+###########################
+library(ggplot2)
+
+
 c = (240 * pi)^(1/3)
 
 
@@ -39,5 +45,20 @@ L = ( (3 * pi ) / ( 4 * p * d ) )^(1/3)
 L
 
 
-# TODO: sample p, d and plot L
+############################
+# sample p, d and plot L
+############################
+
+# sample from uniform distribution for log10(p)
+p_dist_log = runif(n = 1000, min = log10(1e-30), max = log10(1e-3))
+p_dist = 10^p_dist_log
+
+d = 1/320
+
+L_dist = ( (3 * pi ) / ( 4 * p_dist * d ) )^(1/3)
+
+L_dist
+
+# plot L log
+ggplot2::qplot( log10( L_dist) )
 
