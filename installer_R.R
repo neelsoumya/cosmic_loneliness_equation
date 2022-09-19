1ab68f1e1c6b022811f7e57f201850436ac95735
soumyabanerjee <soumyabanerjee@soumyabanerjees-MacBook-Pro.local>
2022-09-19 08:41:50 +0100

    edits

---
+++ installer_R.R
@@ -0,0 +1,8 @@
+#####################################
+# script to install all R packages
+#####################################
+
+install.packages('ggplot2')
+install.packages('rmarkdown')
+install.packages('knitr')
+install.packages('shiny')
