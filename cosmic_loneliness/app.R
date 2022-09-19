20b0b85bcd32d0b0dc5f5c2955f9ca352fa63cc1
soumyabanerjee <soumyabanerjee@soumyabanerjees-MacBook-Pro.local>
2022-09-19 10:00:44 +0100

    edits to code

---
+++ cosmic_loneliness/app.R
@@ -0,0 +1,118 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Cosmic Loneliness Equation"),
+
+    #p_log_MIN = log10(1e-30) # minimum value of p on log scale
+    #p_log_MAX = log10(1e-3)  # maximum value of p on log scale
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+          
+            sliderInput("bins",
+                        "Maximum value of p (probability of another star developing an advanced radio-transmitting civilization):",
+                        min = (1e-30), #1,
+                        max = (1e-3), #50,
+                        value = 1e-10, 
+                        animate = TRUE
+                        )
+            ,
+            
+            #sliderInput("min",
+            #            "Minimum value of p:",
+            #            min = (1e-35), #1,
+            #            max = (1e-30), #50,
+            #            value = 1e-30, 
+            #            animate = TRUE
+            #)
+            #,
+            
+            sliderInput("d",
+                        "Maximum value of d (stellar density in our neighbourhood):",
+                        min = (1/320), #1,
+                        max = (100/320), #50,
+                        value = 1/320, 
+                        animate = TRUE
+            )
+            ,
+            
+            sliderInput("samples",
+                        "Number of samples to draw:",
+                        min = 100, #1,
+                        max = 100000, #50,
+                        value = 1000, 
+                        animate = TRUE
+            )
+            
+                    ),
+        
+        
+        
+            
+
+                  # Show a plot of the generated distribution
+                  mainPanel(
+                     plotOutput("distPlot")
+                  )
+    )
+
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        # x    <- faithful[, 2]
+        # bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        #hist(x, breaks = bins, col = 'darkgray', border = 'white',
+        #     xlab = 'Waiting time to next eruption (in mins)',
+        #     main = 'Histogram of waiting times')
+        
+        ############################
+        # sample p, d and plot L
+        ############################
+        
+        # sample from uniform distribution for log10(p)
+        N_samples = input$samples # 1000 # number of samples
+        p_log_MIN = log10(1e-30) # minimum value of p on log scale
+        p_log_MAX = input$bins # log10(1e-3)  # maximum value of p on log scale
+        d = input$d #1/320 # stellar density in our neighbourhood
+        
+        # sample
+        p_dist_log = runif(n = N_samples, min = p_log_MIN, max = p_log_MAX)
+        p_dist = 10^p_dist_log
+        
+        
+        # cosmic loneliness equation
+        L_dist = ( (3 * pi ) / ( 4 * p_dist * d ) )^(1/3)
+        
+        L_dist
+        
+        # plot L log
+        ggplot2::qplot( log10( L_dist), 
+                        xlab = 'log10 lifetime of human civilization',
+                        ylab = 'Frequency'
+                      )
+        
+        
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
